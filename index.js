5746530bab37fee2cbb6d3e0c135f18ddec81c17
faizan965 <140983174+faizan965@users.noreply.github.com>
2023-08-04 02:55:57 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+//Store a person’s name in a variable
+let personName = "Faizan";
+//print the name in lowercase
+console.log(personName.toLowerCase());
+//print the name in uppercase
+console.log(personName.toUpperCase());
+//print the name in titlecase
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
---
+++ index.ts
@@ -0,0 +1,15 @@
+//Store a person’s name in a variable
+
+let personName : string = "Faizan";
+
+//print the name in lowercase
+
+console.log(personName.toLowerCase());
+
+//print the name in uppercase
+
+console.log(personName.toUpperCase());
+
+//print the name in titlecase
+
+console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
